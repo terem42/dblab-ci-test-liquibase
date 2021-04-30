fbc19405a42cb681a1eb5e152d65095e2c02ae48
Vitaliy Kukharik <vitabaks@gmail.com>
2021-04-30 21:47:29 +0300

    Create 00080_alter_with_long_lock.sql

---
+++ dbmigration/sql/00080_alter_with_long_lock.sql
@@ -0,0 +1,10 @@
+--liquibase formatted sql
+--changeset vitaliy:00080_alter_with_long_lock splitStatements:true endDelimiter:;
+
+BEGIN;
+
+ALTER TABLE hero_data.hero DROP CONSTRAINT pk_hero_id;
+
+select pg_sleep(5);
+
+ROLLBACK;
